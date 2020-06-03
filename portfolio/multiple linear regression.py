97442318df44485f95eaa1cede25b4789b9783a4
Ojasvi Asthana <63544271+ojasviasthana2@users.noreply.github.com>
2020-06-03 20:18:07 +0530

    Update multiple linear regression.py

--- portfolio/multiple linear regression.py
+++ portfolio/multiple linear regression.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
 # print("woohooo")
 
 dataset = pd.read_csv('50_Startups.csv')
@@ -9,15 +11,10 @@ X = dataset.iloc[:,:-1].values  # [rows,col]
 y = dataset.iloc[:,-1].values
 
 # Encoding categorical data. State column is not numerical like other columns.
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-
 ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[3])],remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 
-from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=1/3,random_state=0)
-
 
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
