5fc343de12aa96138881bf2029ca31eff68b0fd4
Ojasvi Asthana <63544271+ojasviasthana2@users.noreply.github.com>
2020-06-03 20:20:09 +0530

    Update multiple linear regression.py

--- portfolio/multiple linear regression.py
+++ portfolio/multiple linear regression.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 # print("woohooo")
 
 dataset = pd.read_csv('50_Startups.csv')
@@ -16,12 +17,9 @@ X = np.array(ct.fit_transform(X))
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=1/3,random_state=0)
 
-from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train,y_train)
 
 y_pred = regressor.predict(X_test)
-
-plt.scatter(X_train, y_train, color='red')
-plt.plot(X_train, regressor.predict(X_train), color='blue')
-plt.show()
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
